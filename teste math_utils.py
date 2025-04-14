794ff6916f63b550d3ce83ee2b95644e3eac0718
N3koVox <bryan.cocav@gmail.com>
2025-04-14 19:48:11 -0300

    Requirements e teste foram feitos

---
+++ teste math_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from math_utils import add, subtract
+
+class TestMathUtils(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(4,6),10)
+        self.assertEqual(add(-2,2),0)
+
+    def test_subtract(self):
+        self.assertEqual(add(7,4),3)
+        self.assertEqual(add(0,0),0)
+
+if __name__ == '__main__':
+    unittest.main()
